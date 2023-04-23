2b9a739743160a37ada97f2182e2d03d70f0b918
sjoshi-1 <200455953@student.georgianc.on.ca>
2023-04-23 12:30:02 -0400

    login and registrartion page

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Login Page</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+<form action="login.php" method="post">
+<h2>LOGIN</h2>
+     	<?php if (isset($_GET['error'])) { ?>
+     		<p class="error"><?php echo $_GET['error']; ?></p>
+     	<?php } ?>
+    
+
+
+<label>User Name</label>
+<input type="text" name="uname" placeholder="Enter your name"
+>
+
+<label>Password</label>
+<input type="password" name="password" placeholder="Enter your password"
+>
+
+<button type="submit">  Login</button>
+
+<a href="signup.php" class="ca">Create an account</a>
+</form>
+</body>
+</html>

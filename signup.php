da78833caa3303d0552b7c6cc70768f9cb22301f
sjoshi-1 <200455953@student.georgianc.on.ca>
2023-04-23 14:10:32 -0400

    commenting in signup page

--- signup.php
+++ signup.php
@@ -5,9 +5,15 @@
 	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
+
+<!--validation of form input will be done in signup-check file-->
      <form action="signup-check.php" method="post">
      	<h2>SIGN UP</h2>
-     	<?php if (isset($_GET['error'])) { ?>
+<!--isset function is set here if there is any error occur in the functioning it will show error message and if everything seems okay it will show success message
+isset is used so that there must be something declared or shown to make it user friendly
+-->
+
+          <?php if (isset($_GET['error'])) { ?>
      		<p class="error"><?php echo $_GET['error']; ?></p>
      	<?php } ?>
 
@@ -27,6 +33,7 @@
                       placeholder="Name"><br>
           <?php }?>
 
+          <!--isset is used to get the value from form , the value will get stored in _get and if value is not filled it will show the same form input-->
           <label>User Name</label>
           <?php if (isset($_GET['uname'])) { ?>
                <input type="text" 
@@ -51,6 +58,7 @@
                  placeholder="Re_Password"><br>
 
      	<button type="submit">Sign Up</button>
+          <!--after clicking the signup button the data will get stored in database -->
           <a href="index.php" class="ca">Already have an account?</a>
      </form>
 </body>
